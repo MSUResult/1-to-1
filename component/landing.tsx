bcab09375071062cecd2072144a65450a0a9453a
MSUResult <adarshrajput9040@gmail.com>
2025-11-07 19:54:45 +0530

    Initial commit from Create Next App

---
+++ component/landing.tsx
@@ -0,0 +1,23 @@
+export default function Landing() {
+  return (
+    <main>
+      <p className="rounded-[16px] p-3 text-white ">
+        🎓100+ Students Trained | 15+ Workshops Delivered
+      </p>
+
+      <h1>
+        Learn React & JavaScript <span> 1-to-1 from a Real Devloper</span>
+      </h1>
+
+      <p>
+        Master modern web development with personalized mentorship. From
+        beginner to advanced, I'll guide you every step of the way.
+      </p>
+
+      <div>
+        <button className="p-2 px-4 ">Book Free Demo</button>
+        <button>View Course</button>
+      </div>
+    </main>
+  );
+}
